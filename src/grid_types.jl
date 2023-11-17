f6ec04416ebb8d86d293603ca8ef9f05c21628ae
azraq27 <bill.gross@me.com>
2023-11-16 20:54:17 -0600

    updated docs

--- src/grid_types.jl
+++ src/grid_types.jl
@@ -12,6 +12,13 @@ struct RectGrid <: Grid
 	spacing::Float64
 end
 
+"""
+	RectGrid(columns::Int,rows::Int;spacing=4.0,trim=[:,:])
+
+Create a rectangular grid with given number of columns and rows. Spacing is given
+in millimeters. The optional argument `trim` can be used to select only a portion
+of the grid.
+"""
 RectGrid(columns::Int,rows::Int;spacing=4.0,trim=[:,:]) = RectGrid(reverse(reshape(1:(columns*rows),(columns,rows))',dims=2)[trim...],spacing)
 
 struct SquareGrid <: Grid
